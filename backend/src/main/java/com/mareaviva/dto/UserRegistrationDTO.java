62aace4236156670ef095acc6932cc3942d6041a
Emagmunioz <emagmunioz@gmail.com>
2025-04-14 12:49:25 +0200

    feat: add UserRegistrationDTO with validation annotations

---
+++ backend/src/main/java/com/mareaviva/dto/UserRegistrationDTO.java
@@ -0,0 +1,102 @@
+package com.mareaviva.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class UserRegistrationDTO {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String province;
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{8}[A-Za-z]$", message = "DNI must be 8 digits followed by a letter")
+    private String dni;
+
+    @NotBlank
+    @Size(min = 9, max = 15)
+    private String phone;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    // Getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
