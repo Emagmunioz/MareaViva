77f9a917c97f3e6a8c69f03155f13723894bb990
Emagmunioz <emagmunioz@gmail.com>
2025-04-14 12:15:25 +0200

    add changes of backend

---
+++ backend/src/test/java/com/mareaviva/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mareaviva;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTests {
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
