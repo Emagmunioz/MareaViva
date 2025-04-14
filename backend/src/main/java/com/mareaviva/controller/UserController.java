e45f186c63810acd338336045a5bf269bb0582d9
Emagmunioz <emagmunioz@gmail.com>
2025-04-14 13:20:25 +0200

    refactor: inject UserService into UserController and delegate registration

--- backend/src/main/java/com/mareaviva/controller/UserController.java
+++ backend/src/main/java/com/mareaviva/controller/UserController.java
@@ -1,5 +1,5 @@
 package com.mareaviva.controller;
-
+import com.mareaviva.service.UserService;
 import com.mareaviva.dto.UserRegistrationDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -9,12 +9,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    // Constructor con inyección de dependencias
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+
 
     @PostMapping
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationDTO userDto) {
-        // Aquí luego se llamará a UserService para guardar en la base de datos
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationDTO userDto){ userService.registerUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
     }
-}
+        // Aquí luego se llamará a UserService para guardar en la base de datos
+       
+    }
+
 
 
