ca28881880b6d520f8b6419c147f51b940fae064
Emagmunioz <emagmunioz@gmail.com>
2025-04-21 14:14:05 +0200

    some changes to repair mistakes

--- backend/src/main/java/com/mareaviva/Application.java
+++ backend/src/main/java/com/mareaviva/Application.java
@@ -1,5 +1,4 @@
 package com.mareaviva;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+    } 
 }
--- backend/src/main/java/com/mareaviva/controller/UserController.java
+++ backend/src/main/java/com/mareaviva/controller/UserController.java
@@ -1,5 +1,6 @@
+
 package com.mareaviva.controller;
-import com.mareaviva.service.UserService;
+
 import com.mareaviva.dto.UserRegistrationDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -7,25 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/users")
 public class UserController {
-    private final UserService userService;
 
-    // Constructor con inyección de dependencias
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-
-
-
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationDTO userDto){ userService.registerUser(userDto);
+    @PostMapping
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationDTO userDto) {
+        // Aquí luego se llamará a UserService para guardar en la base de datos
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
     }
-        // Aquí luego se llamará a UserService para guardar en la base de datos
-       
-    }
-
-
-
+}
